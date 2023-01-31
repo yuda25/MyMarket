a3f5f977ae7515729b1d358899ede3eee53101e2
yuda25 <yuda7246@gmail.com>
2023-01-31 14:05:53 +0700

    first commit

---
+++ src/main/java/com/mymarket/MyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.mymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymarket/MyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
